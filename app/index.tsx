320cedbfba1d508d1005cf5e042245c6b25e206a
vensah-dev <venkateshdeven14@gmail.com>
2024-08-18 22:08:42 +0800

    NativeWind

--- app/index.tsx
+++ app/index.tsx
@@ -2,11 +2,12 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import {Link} from 'expo-router'
+import "../global.css"
 
 export default function App(){
   return (
-    <View style={styles.main}>
-      <Text style={styles.text}>Hello, world!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Hello, world!</Text>
       <StatusBar style='auto'/>
 
       <Link href="/profile" style={{color: 'blue'}}>Profile</Link>
