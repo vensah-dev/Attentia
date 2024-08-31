bb3241243118022ee5054041da97f4a3ba73eee0
vensah-dev <venkateshdeven14@gmail.com>
2024-08-31 17:41:13 +0800

    Updates to one link for all expo people to use

--- app/profile.jsx
+++ app/profile.jsx
@@ -22,7 +22,7 @@ export default function ProfileScreen(){
           <HeaderPlain title={"Profile"}/>
 
 
-          <View className="flex-1 justify-center items-center w-full px-[24px] rounded-[8px] mt-[34px]">
+          <View className="flex-1 justify-center items-center w-full px-6 rounded-[8px] pt-12">
 
             <Image
               source={images.pfpImpStat} // Replace with your image URL
@@ -31,9 +31,9 @@ export default function ProfileScreen(){
               className="flex-1 w-full h-full rounded-[8px] border-white5 border-2"
             />
 
-            <View className="flex-1 items-center absolute self-start ml-12 bottom-4">
+            <View className="flex-1 items-center absolute self-start ml-12 bottom-[4%]">
               <Avatar
-                size={"xlarge"}
+                size={176}
                 rounded
                 backgroundColor={colors.white100}
                 overlayContainerStyle={{backgroundColor: colors.white100}}
@@ -41,16 +41,15 @@ export default function ProfileScreen(){
                 activeOpacity={0.7}
               />
 
-              <Text className="text-white75 font-pmedium text-h6 pt-2">UserName</Text>
+              <Text className="text-white75 font-pmedium text-h6 pt-[2%]">UserName</Text>
             </View>
 
-            <View className="flex-1 items-center absolute self-end right-14">
+            <View className="flex-1 items-center absolute self-end right-[14%]">
               <Text className="text-white100 font-pextrabold text-h1">30%</Text>
               <Text className="text-white100 font-pregular text-kitmin">Improvement in focus</Text>
             </View>
 
           </View>
-
 
         </ScrollView>
       </SafeAreaView>
